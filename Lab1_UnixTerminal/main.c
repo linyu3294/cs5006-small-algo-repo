d88c81650567ea81d50ce116dbb381627917c726
Yu Lin <lin.yu3@husky.neu.edu>
2020-01-10 21:38:07 -0500

    Adding  changes to main.c

--- Lab1_UnixTerminal/main.c
+++ Lab1_UnixTerminal/main.c
@@ -1,1 +1,22 @@
+
 // Add your program here!
+//
+#include <stdio.h>
+
+double power (double base, double n) {
+	double result = base;
+	int i = 0;
+
+	for (i =1; i<n+1; i++) {
+	result = result * base;
+	}
+	return result;
+}
+
+int main() {
+	int i =0;
+	for (i =1; i<11; i++) {
+	int result = power (10, i);
+	printf ("The %dth power of 10 is %d\n" , i,  result);  
+	}
+}
