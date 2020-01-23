60644d79b51f28abe6f6ac9fb56ba8470b5ba83d
linyu3294 <lin.yu3@husky.neu.edu>
2020-01-22 22:50:16 -0500

    Updated myStack.h

--- Assignment2_C_DataStructures/Part2/mystack.h
+++ Assignment2_C_DataStructures/Part2/mystack.h
@@ -6,7 +6,7 @@
 // - Implement each of the functions to create a working stack.
 // - Do not change any of the function declarations
 //   - (i.e. stack_t* create_stack() should not have additional arguments)
-// - You should not have any 'printf' statements in your stack functions. 
+// - You should not have any 'printf' statements in your stack functions.
 //   - (You may consider using these printf statements to debug, but they should be removed from your final version)
 // ==================================================
 #ifndef MYSTACK_H
@@ -17,21 +17,22 @@
 // (i.e. capacity cannot exceed MAX_DEPTH for any stack)
 # define MAX_DEPTH 32
 
+
 // Create a node data structure to store data within
 // our stack. In our case, we will stores 'integers'
 typedef struct node{
-	int data;
-	struct node* next;
+    int data;
+    struct node* next;
 }node_t;
 
 // Create a stack data structure
 // Our stack holds a single pointer to a node, which
 // is a linked list of nodes.
 typedef struct stack{
-	int count;		// count keeps track of how many items
-				// are in the stack.
-	unsigned int capacity;	// Stores the maximum size of our stack
-	node_t* head;		// head points to a node on the top of our stack.
+    int count;        // count keeps track of how many items
+                // are in the stack.
+    unsigned int capacity;    // Stores the maximum size of our stack
+    node_t* head;        // head points to a node on the top of our stack.
 }stack_t;
 
 // Creates a stack
@@ -40,11 +41,17 @@ typedef struct stack{
 // (Think about what the means in terms of memory allocation)
 // The stacks fields should also be initialized to default values.
 // Returns NULL if we couldnt allocate memory.
-stack_t* create_stack(unsigned int capacity){
-	// Modify the body of this function as needed.
-	stack_t* myStack = NULL;	
-
-	return myStack;
+stack_t* create_stack(unsigned int _capacity){
+    // Modify the body of this function as needed.
+    stack_t* myStack = NULL;
+    myStack = (stack_t*)malloc(sizeof(stack_t));
+    if (myStack == NULL){
+        return NULL;
+    }
+    myStack->capacity = _capacity;
+    myStack->count = 0;
+    myStack->head = NULL;
+    return myStack;
 }
 
 // Stack Empty
@@ -53,8 +60,15 @@ stack_t* create_stack(unsigned int capacity){
 // Returns 0 if false (the stack has at least one element)
 // Returns -1 if the stack is NULL.
 int stack_empty(stack_t* s){
-
-	return 0;
+    if (s == NULL){
+        return -1;
+    }
+    if (s->count == 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 // Stack Full
@@ -63,40 +77,84 @@ int stack_empty(stack_t* s){
 // Returns 0 if false (the Stack has more space available to add items)
 // Returns -1 if the stack is NULL.
 int stack_full(stack_t* s){
-
-	return 0;
+    if (s == NULL) {
+        return -1;
+    }
+    if (s-> count == s->capacity){
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
+
 // Push a new item
 // i.e. push a new item into our data structure
-// Returns a 0 if the operation fails 
+// Returns a 0 if the operation fails
 // Returns a 1 if the operation suceeds
 // Returns -1 if the stack is NULL.
 // (i.e. if the stack is full that is an error).
 int stack_enqueue(stack_t* s, int item){
-		return -1; // Note: you should have two return statements in this function.
+    if (s==NULL){
+        return -1;
+    }
+    if (stack_full(s)==1){
+        return 0;
+    }
+    node_t* new_node = (node_t*)malloc(sizeof(node_t));
+    if (new_node == NULL){
+        return -1;
+    }
+    new_node->next = s->head;
+    new_node->data = item;
+    s->head =new_node;
+    s->count++;
+    return 1;
 }
+
 // Dequeue an item
 // Returns the item at the front of the stack and
 // removes an item from the stack.
 // Removing from an empty stack should do nothing
 // Returns -1 if the stack is NULL. Assumption there is not going to be negative numbers in the stack
 int stack_dequeue(stack_t* s){
-
-		return 9999999; // Note: This line is a 'filler' so the code compiles.
+    if (s == NULL){
+        return -1;
+    }
+    if (stack_empty(s)==1){
+        return 0;
+    }
+    node_t* temp = s->head;
+    int ele = temp->data;
+    s->head = temp->next;
+    s->count--;
+    free(temp);
+    return ele; // Note: This line is a 'filler' so the code compiles.
 }
 
 // Stack Size
 // Queries the current size of a stack
 // Returns -1 if the stack is NULL.
 unsigned int stack_size(stack_t* s){
-	return 0;
+    if (s == NULL){
+        return -1;
+    }
+    return (s-> count);
 }
 
 // Free stack
 // Removes a stack and ALL of its elements from memory.
 // This should be called before the proram terminates.
 void free_stack(stack_t* s){
-
+    node_t* itr = s->head;
+    node_t* next;
+    while (itr != NULL) {
+        next = itr->next;
+        free(itr);
+        itr = next;
+    }
+    free(s);
 }
 
 
