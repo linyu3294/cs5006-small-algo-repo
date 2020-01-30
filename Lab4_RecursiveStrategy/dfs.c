3f41a5c80063f3de79923bc5b2d599c4b8675015
linyu3294 <lin.yu3@husky.neu.edu>
2020-01-30 11:52:37 -0500

    Completed dis code

--- Lab4_RecursiveStrategy/dfs.c
+++ Lab4_RecursiveStrategy/dfs.c
@@ -9,14 +9,57 @@ typedef struct TreeNode{
   char data;
 }TreeNode_t;
 
+
+// Helper function - helps to verify that next Left Node exists
+// IF Left does not exist/ == NULL then return -1.
+// IF Left does exist returns 1.
+
+int hasLeft (TreeNode_t* node) {
+    if (node->left == NULL) {
+        return -1;
+    }
+    else { return 1;}
+    return 0;
+}
+
+
+int hasRight (TreeNode_t* node) {
+    if (node->right == NULL) {
+        return -1;
+    }
+    else { return 1;}
+    return 0;
+}
+
+
+
 // Depth-First Tree Traversal
 // Make sure to print out each node you visit, and only print it out once!
 // **Hint** Think recursively
 void dfs(TreeNode_t* start){
     // TODO: Hmm, how can I look through both branches of the node?
     //       Hmm, when do I stop?
-    //
+
+    //Check that starting node is not null and print data stored.
+    if (start == NULL) {
+        printf("\nYour starting node is NULL\n");
+    }
+    else {printf("\nCurrent Node data is %c\n" , start->data);}
+    
+    
+    int leftExist = hasLeft(start);
+    int rightExist = hasRight(start);
+    // Recursive Strategy
+ 
+    if (leftExist == 1){
+        dfs(start->left);
+    }
+    if (rightExist == 1){
+        dfs(start ->right);
+    }
+        
 }
+
 
 int main(){
 
@@ -31,7 +74,7 @@ int main(){
   TreeNode_t* g= malloc(sizeof(TreeNode_t));
   TreeNode_t* h= malloc(sizeof(TreeNode_t));
 
-  // The root of our tree  
+  // The root of our tree
   // The root also has two 'branches' a and b.
   start->data = 's';
   start->left = a;
@@ -52,7 +95,7 @@ int main(){
   // All of the 'leaves' in our tree
   d->data = 'd';
   d->left = NULL;
-  d->right = NULL;   
+  d->right = NULL;
   
   e->data = 'e';
   e->left = NULL;
@@ -87,4 +130,3 @@ int main(){
   
   return 0;
 }
-
