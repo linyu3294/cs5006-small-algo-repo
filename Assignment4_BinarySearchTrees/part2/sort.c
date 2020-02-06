e39889efdd4b176aa3fe34f232bd5cc1e94699d1
linyu3294 <lin.yu3@husky.neu.edu>
2020-02-06 13:30:05 -0500

    completed sort.c

--- Assignment4_BinarySearchTrees/part2/sort.c
+++ Assignment4_BinarySearchTrees/part2/sort.c
@@ -5,11 +5,71 @@
 
 // =============== Helper Functions ===============
 
-// Merges two subarrays of arr[]. 
-// First subarray is arr[l..m] 
-// Second subarray is arr[m+1..r] 
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
 void merge(int arr[], int l, int m, int r) {
   //TODO: implement merge.
+    if (arr == NULL) {
+        return;
+    }
+    int h_1 = m - l +1;
+    int h_2 = r - m;
+    
+    int left[h_1];
+    int right[h_2];
+    
+    int i =0;
+    int j =0;
+    for (int i=0; i<h_1; i++){
+        left[i] = arr[l + i];
+    }
+    for (int j=0; j<h_2; j++){
+        right[j] = arr[(m +1 ) + j];
+    }
+    
+    i =0;
+    j =0;
+    int k = l;
+    
+    while (i<h_1 && j <h_2){
+        if (left[i] <= right[j]){
+            arr[k] = left[i];
+            ++i;
+        }
+        else {
+            arr[k] = right[j];
+            ++j;
+        }
+        k++;
+    }
+    
+    
+    while (i< h_1){
+        arr[k] = left[i];
+        ++i;
+        ++k;
+    }
+    
+    while (j< h_2){
+        arr[k] = right[j];
+        ++j;
+        ++k;
+    }
+}
+
+// This is a helper function used in sortIntegers.
+void mergeSort(int* arr, int first, int last){
+    if (arr == NULL){
+        return;
+    }
+    if (first == last){
+        return;
+    }
+    int middle = (first + last)/2;
+    mergeSort(arr, first, middle);
+    mergeSort(arr, middle +1 , last);
+    merge(arr, first, middle, last);
 }
 
 // Provided below is a sort function. We have also
@@ -17,23 +77,29 @@ void merge(int arr[], int l, int m, int r) {
 // to help organize your code.
 // Name: sortIntegers
 // Input(s):
-//          (1) 'array' is a pointer to an integer address. 
+//          (1) 'array' is a pointer to an integer address.
 //              This is the start of some 'contiguous block of memory' that we will sort.
 //          (2) 'size' tells us how big the array of data is we are sorting.
 // Output: No value is returned, but 'array' should be modified to store a sorted array of numbers.
 void sortIntegers(int* array, unsigned int size){
-    // TODO: Implement merge sort
+    if (array == NULL){
+        return;
+    }
+    mergeSort(array, 0, size -1);
 }
 
 
 // Input: A pointer to an array (i.e. the array itself points to the first index)
 //        The size of the array (Because we do not know how big the array is automatically)
 void printIntArray(int* array, unsigned int size){
-  unsigned int i; // Note: 'unsigned int' is a datatype for storing positive integers.
-  for(i = 0; i < size; i=i+1){
+    if (array == NULL) {
+        return;
+    }
+    unsigned int i; // Note: 'unsigned int' is a datatype for storing positive integers.
+    for(i = 0; i < size; i++){
     printf("%d ",array[i]);
-  }
-  printf("\n");
+    }
+    printf("\n");
 }
 
 int main(){
